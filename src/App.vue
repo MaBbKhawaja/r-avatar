350dc2a787964e08fd3a151bb83b18c0abe93ac8
MaBbKhawaja <anees0003@gmail.com>
2024-09-05 15:34:05 +0100

    typescript error removed

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Avatar from './components/Avatar.vue'
   <Avatar name="h z" :size="200" />
   <Avatar name="m z" :size="200" />
   <Avatar name="z z" :size="200" :radius="10" />
-  <Avatar name="x M" :size="200" :radius="10" color="red" fontSize="300" />
+  <Avatar name="x M" :size="200" :radius="10" color="red" />
   <Avatar name="m a-b b" :size="48" />
   <Avatar name="h z" :radius="10" :size="200" image="https://amymhaddad.s3.amazonaws.com/morocco-blue.png" />
 
